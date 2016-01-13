a13a1cd8c0170987caafaab0144efc53cac2547b
onysko <pavlo.onysko@gmail.com>
2016-01-13 12:26:42 +0200

    Updated test

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -7,13 +7,22 @@
  */
 namespace samsonframework\routing\tests;
 
+use samsonframework\routing\Core;
 use samsonframework\routing\Generator;
 use samsonframework\routing\generator\Structure;
 use samsonframework\routing\Route;
 use samsonframework\routing\RouteCollection;
 
+
 class GeneratorTest extends \PHPUnit_Framework_TestCase
 {
+    /** Routing function wrapper */
+    public function routerLogic($path, $method, $functionName = Core::ROUTING_LOGIC_FUNCTION)
+    {
+        $path = rtrim(strtok($method.'/'.ltrim($path, '/'), '?'), '/');
+        return $functionName($path, $method);
+    }
+    
     public function testGeneration()
     {
         // Create routes descriptions with identifiers
@@ -45,79 +54,78 @@ class GeneratorTest extends \PHPUnit_Framework_TestCase
         }
 
         $generator = new Structure($routes, new \samsonphp\generator\Generator());
-        $routerLogicFunction = '__router'.rand(0, 1000);
-        $routerLogic = $generator->generate($routerLogicFunction);
+        $routerLogic = $generator->generate();
 
         // Create real file for debugging
         file_put_contents(__DIR__.'/testLogic.php', '<?php '."\n".$routerLogic);
         require(__DIR__.'/testLogic.php');
 
-        $result = $routerLogicFunction('/', Route::METHOD_GET);
+        $result = $this->routerLogic('/', Route::METHOD_GET);
         $this->assertEquals('main-page', $result[0]);
 
-        $result = $routerLogicFunction('/', Route::METHOD_POST);
+        $result = $this->routerLogic('/', Route::METHOD_POST);
         $this->assertEquals(null, $result[0]);
 
-        $result = $routerLogicFunction('/user/winners/', Route::METHOD_GET);
+        $result = $this->routerLogic('/user/winners/', Route::METHOD_GET);
         $this->assertEquals('user-winners-slash', $result[0]);
 
-        $result = $routerLogicFunction('/123', Route::METHOD_GET);
+        $result = $this->routerLogic('/123', Route::METHOD_GET);
         $this->assertEquals('inner-page', $result[0]);
         $this->assertArrayHasKey('page', $result[1]);
 
-        $result = $routerLogicFunction('/123/23123', Route::METHOD_GET);
+        $result = $this->routerLogic('/123/23123', Route::METHOD_GET);
         $this->assertEquals('two-params', $result[0]);
 
-        $result = $routerLogicFunction('/user/123', Route::METHOD_GET);
+        $result = $this->routerLogic('/user/123', Route::METHOD_GET);
         $this->assertEquals('user-by-id', $result[0]);
         $this->assertArrayHasKey('id', $result[1]);
 
-        $result = $routerLogicFunction('/user/123/form', Route::METHOD_GET);
+        $result = $this->routerLogic('/user/123/form', Route::METHOD_GET);
         $this->assertEquals('user-by-id-form', $result[0]);
         $this->assertArrayHasKey('id', $result[1]);
 
-        $result = $routerLogicFunction('/user/123/friends', Route::METHOD_GET);
+        $result = $this->routerLogic('/user/123/friends', Route::METHOD_GET);
         $this->assertEquals('user-by-id-friends', $result[0]);
         $this->assertArrayHasKey('id', $result[1]);
 
-        $result = $routerLogicFunction('/user/male/18+', Route::METHOD_GET);
+        $result = $this->routerLogic('/user/male/18+', Route::METHOD_GET);
         $this->assertEquals('user-by-gender-age', $result[0]);
         $this->assertArrayHasKey('gender', $result[1]);
         $this->assertArrayHasKey('age', $result[1]);
 
-        $result = $routerLogicFunction('/user/123/friends/321', Route::METHOD_GET);
+        $result = $this->routerLogic('/user/123/friends/321', Route::METHOD_GET);
         $this->assertEquals('user-by-id-friends-with-id', $result[0]);
         $this->assertArrayHasKey('id', $result[1]);
         $this->assertArrayHasKey('groupid', $result[1]);
 
-        $result = $routerLogicFunction('/friend/123/form', Route::METHOD_GET);
+        $result = $this->routerLogic('/friend/123/form', Route::METHOD_GET);
         $this->assertEquals('entity-by-id-form', $result[0]);
         $this->assertArrayHasKey('entity', $result[1]);
         $this->assertArrayHasKey('id', $result[1]);
 
-        $result = $routerLogicFunction('/123/test/1', Route::METHOD_GET);
+        $result = $this->routerLogic('/123/test/1', Route::METHOD_GET);
         $this->assertEquals('entity-by-id-form-test', $result[0]);
         $this->assertArrayHasKey('id', $result[1]);
         $this->assertArrayHasKey('page', $result[1]);
 
         // We consider that all routes are having slash at the end
-        $result = $routerLogicFunction('/user/', Route::METHOD_GET);
+        $result = $this->routerLogic('/user/', Route::METHOD_GET);
         $this->assertEquals('user-home-without-slash', $result[0]);
 
-        $result = $routerLogicFunction('/user', Route::METHOD_GET);
+        $result = $this->routerLogic('/user', Route::METHOD_GET);
         $this->assertEquals('user-home-without-slash', $result[0]);
-        
-        $result = $routerLogicFunction('/userlist/', Route::METHOD_GET);
+
+        $result = $this->routerLogic('/userlist/', Route::METHOD_GET);
         $this->assertEquals('test-two-similar-fixed', $result[0]);
 
-        $result = $routerLogicFunction('/user/123/n"$ode', Route::METHOD_GET);
+        $result = $this->routerLogic('/user/123/n"$ode', Route::METHOD_GET);
         $this->assertEquals('user-by-id-node', $result[0]);
 
-        $result = $routerLogicFunction('/user/123/n"$ode/321', Route::METHOD_GET);
+        $result = $this->routerLogic('/user/123/n"$ode/321', Route::METHOD_GET);
         $this->assertEquals('user-by-id-node-with-id', $result[0]);
 
         // Empty parameters are resolved to null route
-        $result = $routerLogicFunction('/user//get', Route::METHOD_GET);
+        $result = $this->routerLogic('/user//get', Route::METHOD_GET);
         $this->assertEquals(null, $result[0]);
     }
 }
