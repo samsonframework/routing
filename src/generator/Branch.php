f74ccf8b28552b15be4b68a399b1d6d62910e298
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-01-14 19:24:13 +0200

    Removed rule 3

--- src/generator/Branch.php
+++ src/generator/Branch.php
@@ -295,18 +295,18 @@ class Branch
              * Rule #3
              * If both branches are not parametrized then branch with shorter pattern string has higher priority.
              */
-            if (strlen($aBranch->nodeValue()) > strlen($bBranch->nodeValue())) {
-                return 1;
-            } elseif (strlen($aBranch->nodeValue()) < strlen($bBranch->nodeValue())) {
-                return -1;
-            } else {
-                /**
-                 * Rule #4
-                 * If both branches are not parametrized and they have two length-equal string patterns then not
-                 * "deeper" branch has priority.
-                 */
-                return $aBranch->size > $bBranch->size ? 1 : -1;
-            }
+//            if (strlen($aBranch->nodeValue()) > strlen($bBranch->nodeValue())) {
+//                return 1;
+//            } elseif (strlen($aBranch->nodeValue()) < strlen($bBranch->nodeValue())) {
+//                return -1;
+//            } else {
+//                /**
+//                 * Rule #4
+//                 * If both branches are not parametrized and they have two length-equal string patterns then not
+//                 * "deeper" branch has priority.
+//                 */
+//                return $aBranch->size > $bBranch->size ? 1 : -1;
+//            }
         }
     }
 }
